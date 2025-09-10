c2fa850c598a74c69bef0f611c1edc2cc369a6ff
Yousefess <yousefe660@gmail.com>
2025-09-10 13:06:10 +0330

    Update content

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from src.config_reader import read_config
 from src.data_ingestion import DataIngestion
+from src.data_processing import DataProcessing
+from src.model_training import ModelTraining
 
 data_ingestion = DataIngestion(read_config("config/config.yaml"))
 data_ingestion.run()
+
+data_processing = DataProcessing(read_config("config/config.yaml"))
+data_processing.run()
+
+model_training = ModelTraining(read_config("config/config.yaml"))
+model_training.run()
